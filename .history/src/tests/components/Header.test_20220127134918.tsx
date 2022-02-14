d23fb66b5dc40346185529333c8612c4a1c2f617
Pavlo Pysaniuk <93548330+pasha2708@users.noreply.github.com>
2022-02-14 21:18:23 +0200

    testing Docker

--- .history/src/tests/components/Header.test_20220127134918.tsx
+++ .history/src/tests/components/Header.test_20220127134918.tsx
@@ -2,7 +2,6 @@ import { shallow } from 'enzyme';
 
 import { Header } from '../../components';
 
-
 describe("Header component", () => {
   it("should render Header and take snapshot", () => {
     const component = shallow(<Header />);
