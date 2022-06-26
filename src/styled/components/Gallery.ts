db1e8c9d69a4e71ec25b8dc02187cbcea84b1510
Mike Gusev <pasha2708@gmail.com>
2022-06-26 19:42:32 +0300

    remove display block

--- src/styled/components/Gallery.ts
+++ src/styled/components/Gallery.ts
@@ -1,18 +1,17 @@
-import { Col, Container } from 'react-bootstrap';
-import styled from 'styled-components';
+import { Col, Container } from "react-bootstrap";
+import styled from "styled-components";
 
 const StyledCol = styled(Col)`
-  img {
-    margin-bottom: 1rem;
-  }
+	img {
+		margin-bottom: 1rem;
+	}
 `;
 
 const StyledContainer = styled(Container)`
-  display: block;
-  text-align: center;
-  img {
-    cursor: pointer;
-  }
+	text-align: center;
+	img {
+		cursor: pointer;
+	}
 `;
 
 export { StyledCol, StyledContainer };
